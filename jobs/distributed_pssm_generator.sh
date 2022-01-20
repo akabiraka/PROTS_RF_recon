d1b80611acb9d34b745f61dc34f62d6d0b56c775
Anowarul Kabir <akabir0101@gmail.com>
2022-01-20 16:57:43 -0500

    bug solved related to job running on argo

--- jobs/distributed_pssm_generator.sh
+++ jobs/distributed_pssm_generator.sh
@@ -4,15 +4,14 @@
 ## --workdir is not used in this file.
 
 #SBATCH --job-name=PROTS_RF_recon
-#SBATCH --qos=csqos
 #SBATCH --output=/scratch/akabir4/PROTS_RF_recon/outputs/argo_logs/distributed_pssm_generator-%N-%j.output
 #SBATCH --error=/scratch/akabir4/PROTS_RF_recon/outputs/argo_logs/distributed_pssm_generator-%N-%j.error
 #SBATCH --mail-user=<akabir4@gmu.edu>
 #SBATCH --mail-type=BEGIN,END,FAIL
 
 #SBATCH --partition=all-HiPri
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=16000MB
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=8000MB
 
 ##for training
 #SBATCH --array=0-202
@@ -22,4 +21,4 @@
 ##the array task is set in the environment variable $SLURM_ARRAY_TASK_ID in python you
 ##can scrape it with ID = int(os.environ["SLURM_ARRAY_TASK_ID"])
 
-python data_generators/distributed_pssm_generator.py
+python data_generators/distributed_pssm.py
